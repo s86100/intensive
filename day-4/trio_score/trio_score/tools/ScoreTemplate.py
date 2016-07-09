a8c830a0520cb9598e808d9a288e0408eea4736f
Josiah Wolf Oberholtzer <josiah.oberholtzer@gmail.com>
2016-07-09 15:09:02 -0700

    Writing day-1 and day-4 curriculum.

--- day-4/trio_score/trio_score/tools/ScoreTemplate.py
+++ day-4/trio_score/trio_score/tools/ScoreTemplate.py
@@ -1,12 +1,47 @@
 # -*- coding: utf-8 -*-
-from abjad import Score, Staff, Voice
-from abjad.tools import abctools
+from abjad import attach
+from abjad import Clef
+from abjad import Score
+from abjad import Staff
+from abjad import StaffGroup
+from abjad import Voice
+from abjad import instrumenttools
 
 
-class ScoreTemplate(abctools.AbjadObject):
+class ScoreTemplate(object):
 
     def __call__(self):
-        voice = Voice(name='Example Voice')
-        staff = Staff([voice], name='Example Staff')
-        score = Score([staff], name='Example Score')
+        # Violin
+        violin_staff = Staff(
+            [Voice(name='Violin Voice')],
+            name='Violin Staff',
+            context_name='ViolinStaff',
+            )
+        attach(Clef('treble'), violin_staff)
+        attach(instrumenttools.Violin(), violin_staff)
+        # Viola
+        viola_staff = Staff(
+            [Voice(name='Viola Voice')],
+            name='Viola Staff',
+            context_name='ViolaStaff',
+            )
+        attach(Clef('alto'), viola_staff)
+        attach(instrumenttools.Viola(), viola_staff)
+        # Cello
+        cello_staff = Staff(
+            [Voice(name='Cello Voice')],
+            name='Cello Staff',
+            context_name='CelloStaff',
+            )
+        attach(Clef('bass'), cello_staff)
+        attach(instrumenttools.Cello(), cello_staff)
+        # Everything else
+        staff_group = StaffGroup(
+            [violin_staff, viola_staff, cello_staff],
+            name='Trio Staff Group',
+            )
+        score = Score(
+            [staff_group],
+            name='Trio Score',
+            )
         return score
--- day-4/trio_score/trio_score/tools/SegmentMaker.py
+++ day-4/trio_score/trio_score/tools/SegmentMaker.py
@@ -1,12 +1,44 @@
 # -*- coding: utf-8 -*-
-from abjad.tools import abctools
+import collections
+from abjad import Measure
+from abjad import MultimeasureRest
+from abjad import Multiplier
+from abjad import TimeSignature
+from abjad import Voice
+from abjad import attach
+from abjad import iterate
+from abjad import mutate
 from abjad.tools import lilypondfiletools
 from trio_score.tools import ScoreTemplate
 
 
-class SegmentMaker(abctools.AbjadObject):
+class SegmentMaker(object):
 
-    def __init__(self):
+    def __init__(
+        self,
+        time_signatures=None,
+        violin_pitches=None,
+        violin_rhythm_maker=None,
+        violin_seed=None,
+        viola_pitches=None,
+        viola_rhythm_maker=None,
+        viola_seed=None,
+        cello_pitches=None,
+        cello_rhythm_maker=None,
+        cello_seed=None,
+        ):
+        time_signatures = [TimeSignature(_) for _ in time_signatures]
+        assert len(time_signatures)
+        self.time_signatures = time_signatures
+        self.violin_pitches = violin_pitches
+        self.violin_rhythm_maker = violin_rhythm_maker
+        self.violin_seed = violin_seed
+        self.viola_pitches = viola_pitches
+        self.viola_rhythm_maker = viola_rhythm_maker
+        self.viola_seed = viola_seed
+        self.cello_pitches = cello_pitches
+        self.cello_rhythm_maker = cello_rhythm_maker
+        self.cello_seed = cello_seed
         self.score_template = ScoreTemplate()
 
     def __call__(
@@ -15,9 +47,51 @@ class SegmentMaker(abctools.AbjadObject):
         previous_segment_metadata=None,
         ):
         score = self.score_template()
-        score['Example Voice'].extend("c'4 ( d'4 e'4 f'4 )")
+        violin_measures = self._make_measures(
+            self.time_signatures,
+            self.violin_rhythm_maker,
+            self.violin_pitches,
+            self.violin_seed,
+            )
+        score['Violin Voice'].extend(violin_measures)
+        viola_measures = self._make_measures(
+            self.time_signatures,
+            self.viola_rhythm_maker,
+            self.viola_pitches,
+            self.viola_seed,
+            )
+        score['Viola Voice'].extend(viola_measures)
+        cello_measures = self._make_measures(
+            self.time_signatures,
+            self.cello_rhythm_maker,
+            self.cello_pitches,
+            self.cello_seed,
+            )
+        score['Cello Voice'].extend(cello_measures)
         lilypond_file = lilypondfiletools.make_basic_lilypond_file(
             score,
             includes=['../../stylesheets/stylesheet.ily'],
             )
         return lilypond_file, segment_metadata
+
+    def _make_measures(self, time_signatures, rhythm_maker, pitches, seed):
+        seed = seed or 0
+        measures = Voice()
+        for time_signature in time_signatures:
+            multimeasure_rest = MultimeasureRest(1)
+            multiplier = Multiplier(time_signature)
+            attach(multiplier, multimeasure_rest)
+            measures.append(Measure(time_signature, [multimeasure_rest]))
+        if rhythm_maker is None:
+            divisions = rhythm_maker(time_signatures, rotation=seed)
+            mutate(measures).replace_measure_contents(divisions)
+        if pitches is not None:
+            if not isinstance(pitches, collections.Iterable):
+                pitches = [pitches]
+            iterator = iterate(measures).by_logical_tie(pitched=True)
+            iterator = enumerate(iterator, seed)
+            for i, logical_tie in iterator:
+                pitch = pitches[i % len(pitches)]
+                for leaf in logical_tie:
+                    leaf.written_pitch = pitch
+        return measures
